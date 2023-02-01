a5be6d262318dc4b80962eb5522f782c7975cc59
zaki ahmed <zakiahmed@zakis-MacBook-Pro.local>
2023-01-31 18:27:20 -0600

    finished prof=d but need to fix port issue

--- server.js
+++ server.js
@@ -1,20 +1,44 @@
+//creating required path's and express
 const express = require("express");
-
-const path = require('path');
-
+const path = require("path");
 const app = express();
-const PORT = 3000
+const PORT = 3001;
+const fs = require("fs");
+const db = require("./db/db.json");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
+//setting up the servers needed
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'./public/index.html'))
-})
-app.get('/notes',(req,res)=>{
-    res.sendFile(path.join(__dirname,'./public/notes.html'))
-})
-app.listen(PORT,()=>{
-    console.log("app is listening")
-})
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(db);
+});
+//psoting the notes from 
+const postNote = (body, notesArr) => {
+  const newNote = body;
+  body.id = notesArr.length;
+  notesArr.push(newNote);
+
+  fs.writeFileSync(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify(notesArr)
+  );
+  return newNote;
+};
+
+app.post("/api/notes", (req, res) => {
+    const newNote = postNote(req.body, db)
+    res.json(true)
+});
+
+app.listen(PORT, () => {
+  console.log("app is listening");
+});
